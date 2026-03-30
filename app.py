e906b0fd89dfb0d7c3d15a4cafb7c5b799311a8b
evelynoyarzun09-png <evelyn.oyarzun.09@gmail.com>
2026-03-30 20:39:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -491,7 +491,7 @@ elif seccion == "Preparación de paciente":
         st.markdown('<div class="titulo-bloque">Imagen</div>', unsafe_allow_html=True)
 
         if PACIENTE_IMG is not None and PACIENTE_IMG.exists():
-            st.image(str(PACIENTE_IMG), width=210)
+            st.image(str(PACIENTE_IMG), width=500)
         else:
             st.info("Guarda la imagen como 'paciente.png' o 'paciente.jpg' en la misma carpeta del app.py.")
 
